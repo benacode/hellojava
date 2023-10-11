b891ff3452bcab6dcb5b12db2939e4571ea6297e
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-10-11 17:12:39 +0300

    Hello this is modified file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main{
   public  static void main(String[] args){
-    System.out.println("Hello welcome to java bena class");
+    System.out.println("Hello welcome to java bena class ....class of git");
   }
 }
